9db84d04113d5dbfe32ab683d1d0c6ed2cc35b80
Omsurvase24 <omsurvase24@gmail.com>
2024-05-16 23:04:14 +0530

    syntax

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo "I love pizza <br>";
+echo "Its very tasty";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <br>
+    <button>Order A Pizza</button>
+</body>
+
+</html>
