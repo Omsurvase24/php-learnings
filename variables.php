f996ce928a054483d970c5a8f4db2df261859de3
Omsurvase24 <omsurvase24@gmail.com>
2024-05-17 03:44:10 +0530

    variables

---
+++ variables.php
@@ -0,0 +1,20 @@
+<?php
+$name = "Bro";
+$age = 21;
+$height = 5.11;
+$employed = false;
+echo "Hello {$name} I am {$age}, my height is {$height} <br>";
+
+echo "Am I emplyed? {$employed} <br>";
+
+$x = 10;
+$y = 2;
+$z = null;
+
+$z = $x + $y;
+//$z = $x - $y;
+//$z = $x * $y;
+//$z = $x / $y;
+//$z = $x ** $y;
+//$z = $x % $y;
+echo $z;
