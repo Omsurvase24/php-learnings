1217549f5a1cf1419d278fe8ba57645f2cfe5308
Omsurvase24 <omsurvase24@gmail.com>
2024-05-17 19:02:56 +0530

    if else statements

---
+++ if_else.php
@@ -0,0 +1,9 @@
+<?php
+$age = 0;
+if ($age <= 18 && $age != 0) {
+    echo "You are not old enough to enter this website";
+} elseif ($age == 0) {
+    echo "Seriously dude?";
+} else {
+    echo "Comeon in Dawgg";
+}
